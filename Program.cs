8fb2a8b37a20bcc34aca9d5296c3108deb65af71
eseni <eseni@DESKTOP-TM01D22>
2023-01-23 22:04:50 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string playerName = "Player";
+            bool onMission = true;
+            char playerEqipRang = 'C';
+            byte playerLevel = 39;
+            uint playerHealth = 100500;
+            int playerEndurance = 100500;
+            double playerDamageResistence = 14435.45;
+            float playerDamage = 17889.12f;
+            ulong quantityCoins = 16998493009422;
+            long quantityCrystals = 134775;
+        }
+    }
+}
